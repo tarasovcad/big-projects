a3bb0b98955aa173bd46836c8b675e8a6cb09585
Maksym <tarasovcad@gmail.com>
2024-02-25 20:38:15 -0600

    feat: add script for a burger-menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
 const burger = document.querySelector('[data-burger]')
+const nav = document.querySelector('[data-nav]')
+const body = document.body
+const navItems = document.querySelectorAll('a')
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('burger--active')
+    nav.classList.toggle('nav--visible')
+    body.classList.toggle('stop-scroll')
+})
+
+navItems.forEach(el => {
+    el.addEventListener('click', () => {
+        burger.classList.remove('burger--active')
+        nav.classList.remove('nav--visible')
+        body.classList.remove('stop-scroll')
+    })
 })
